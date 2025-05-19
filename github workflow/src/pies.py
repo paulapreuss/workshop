b08965a604f819762922d7e4e5aa88b135927f1b
paulapreuss <paulavpreuss@outlook.com>
2025-05-19 15:25:24 +0200

    Move pies into separate module

--- github workflow/src/pies.py
+++ github workflow/src/pies.py
@@ -1,11 +1,10 @@
 from src.utils.cooking_techniques import mix, bake
 
-def banana_cake(*ingredients):
+
+def pekan_pie(*ingredients):
     dough = mix(*ingredients)
     return bake(dough)
 
-def cheesecake(*ingredients):
-    print("watch out for lactose intolerance")
+def key_lime_pie(*ingredients):
     dough = mix(*ingredients)
     return bake(dough)
-
