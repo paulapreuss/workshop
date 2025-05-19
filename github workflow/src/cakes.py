f14a9d0505716d3e8a3f5a8b97c3c9a988e588b3
paulapreuss <paulavpreuss@outlook.com>
2025-05-19 11:10:34 +0200

    Delete duplicated code

--- github workflow/src/cakes.py
+++ github workflow/src/cakes.py
@@ -1,7 +1,6 @@
 from src.utils.cooking_techniques import mix, bake
 
 def banana_cake(*ingredients):
-
     dough = mix(*ingredients)
     return bake(dough)
 
@@ -12,20 +11,8 @@ def cheesecake(*ingredients):
 def pekan_pie(*ingredients):
     dough = mix(*ingredients)
     return bake(dough)
-
-
 
 
 def cake(*ingredients, cake_name=""):
     dough = mix(*ingredients)
     return f"{bake(dough)}for {cake_name}"
-
-def banana_cake(*ingredients):
-
-    return cake(*ingredients, cake_name="banana cake")
-
-def cheesecake(*ingredients):
-    return cake(*ingredients, cake_name="cheesecake")
-
-def pekan_pie(*ingredients):
-    return cake(*ingredients, cake_name="pekan_pie")
