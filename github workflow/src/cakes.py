e38f90c0586e82dd0786f5f7ee2d2b304a53a6a7
paulapreuss <paulavpreuss@outlook.com>
2025-05-19 15:17:53 +0200

    Delete code

--- github workflow/src/cakes.py
+++ github workflow/src/cakes.py
@@ -12,20 +12,3 @@ def cheesecake(*ingredients):
 def pekan_pie(*ingredients):
     dough = mix(*ingredients)
     return bake(dough)
-
-
-
-
-def cake(*ingredients, cake_name=""):
-    dough = mix(*ingredients)
-    return f"{bake(dough)}for {cake_name}"
-
-def banana_cake(*ingredients):
-
-    return cake(*ingredients, cake_name="banana cake")
-
-def cheesecake(*ingredients):
-    return cake(*ingredients, cake_name="cheesecake")
-
-def pekan_pie(*ingredients):
-    return cake(*ingredients, cake_name="pekan_pie")
